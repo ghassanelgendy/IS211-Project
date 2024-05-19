66bd76d3b6e069e9ecdad2cabc4354040c0cca56
Rawan <rawanehab523@gmail.com>
2024-05-19 22:42:51 +0300

    GUI

--- database.py
+++ database.py
@@ -12,8 +12,8 @@ cnxn_str = ("Driver={ODBC Driver 17 for SQL Server};"
 cnxn = pyodbc.connect(cnxn_str)
 root = tk.Tk()
 root.title("University Library Management")
-root.geometry("600x600")
-root.resizable(width=False, height=False)
+root.geometry("840x530")
+root.resizable(False, False)
 root.configure(bg='#2E2E2E')
 
 bg_color = '#2E2E2E'
@@ -28,7 +28,7 @@ style.configure('TButton', background=btn_bg_color, foreground=btn_fg_color, bor
                 font=('Andalus', 17), focuscolor='none')
 style.map('TButton', background=[('active', btn_hover_color)])
 
-title = tk.Label(text="Welcome to our Library!", font=('Baskerville Old Face', 25), bg=bg_color, fg=fg_color)
+title = tk.Label(text="University Library Management System", font=('Baskerville Old Face', 25), bg=bg_color, fg=fg_color)
 title.pack(pady=30)
 
 log_entries = []
@@ -158,6 +158,97 @@ def join_students_books(sid_values, cnxn):
     return concatenated_df
 
 
+def DeleteStud(SID, conn):
+    crsr = conn.cursor()
+    query = f"Delete from Borrows where SID = {SID}"
+    crsr.execute(query)
+    query = f"Delete from Students where SID = {SID}"
+    crsr.execute(query)
+    conn.commit()
+    conn.close()
+
+
+def UpdateStud(SID, Col, Val, conn):
+    if Col == 'SID':
+        conn.close()
+        raise Exception('Cannot Edit Student\'s Id')
+    crsr = conn.cursor()
+    query = f"Update Students set {Col} = '{Val}' where SID = {SID}"
+    crsr.execute(query)
+    conn.commit()
+    conn.close()
+
+
+def UpdateBook(ISBN, Col, Val, conn):
+    if Col == 'ISBN':
+        conn.close()
+        raise Exception('Cannot Edit Book\'s ISBN')
+    crsr = conn.cursor()
+    query = f"Update Books set {Col} = '{Val}' where ISBN = {ISBN}"
+    crsr.execute(query)
+    conn.commit()
+    conn.close()
+
+def Number_of_Books_Borrowed_by_AllStuds(conn):
+    crsr = conn.cursor()
+    query = """
+        Select Students.SID, CONCAT(Students.FirstName, ' ', Students.LastName) as FullName, count(*) as Number_of_Borrowed_Books
+        from Students join Borrows on Students.SID = Borrows.SID
+        group By Students.SID, Students.FirstName, Students.LastName
+    """
+    crsr.execute(query)
+    data = crsr.fetchall()
+    conn.close()
+
+    return data
+
+def Number_of_Books_Borrowed_by_AStud(SID, conn):
+    crsr = conn.cursor()
+    query = f"""
+        select Students.SID, Students.FirstName, Students.LastName, count(*) as BooksBorrowed
+        from Borrows join Students on Students.SID = Borrows.SID
+        GROUP by Students.SID, Students.FirstName, Students.LastName
+        HAVING Students.SID = {SID}
+    """
+    crsr.execute(query)
+    data = crsr.fetchall()
+    conn.close()
+    return data[0] if data else None
+
+def NumberOfBooksWrittenByAllAuthors(conn):
+    crsr = conn.cursor()
+    query = """
+        Select Author.Name, count(*) as Number_of_Books
+        from Author JOIN Books on Author.Name = Books.AuthorName
+        group By Author.Name
+    """
+    crsr.execute(query)
+    data = crsr.fetchall()
+    conn.close()
+    return data
+
+def BooksByAuthor(Name, conn):
+    query = f"""
+        SELECT Author.Name, Books.ISBN, Books.Name, Books.PublisherName, Books.Genre, Books.PublicationYear
+        from Author join Books on Author.Name = Books.AuthorName
+        where Author.Name = '{Name}'
+    """
+    crsr = conn.cursor()
+    crsr.execute(query)
+    data = crsr.fetchall()
+    conn.close()
+    return data
+
+def UpdateAdmin(SSN,Col,Val,conn):
+    if Col == 'SSN':
+      conn.close()
+      raise Exception('Cannot Edit Admin\'s SSN')
+    crsr = conn.cursor()
+    query = f"Update Admins set {Col} = '{Val}' where SSN = {SSN}"
+    crsr.execute(query)
+    conn.commit()
+    conn.close()
+
 def sign_up():
     def admin_signup():
         top = tk.Toplevel(root)
@@ -249,28 +340,33 @@ def sign_up():
 
 def update_user():
     def update_action():
-        user_id = user_id_entry.get()
-        new_details = new_details_entry.get()
-        # You can add the SQL update command here to update user details
-        log_entries.append("User details updated")
-        messagebox.showinfo("Info", "User details updated!")
+        sid = sid_entry.get()
+        column = column_entry.get()
+        value = value_entry.get()
+        UpdateStud(sid, column, value, cnxn)
+        log_entries.append("Student updated")
+        messagebox.showinfo("Info", "Student updated!")
 
     top = tk.Toplevel(root)
-    top.title("Update User")
+    top.title("Update Student")
     top.geometry("400x300")
     top.configure(bg=bg_color)
 
-    tk.Label(top, font=("Andalus", 16), text="User ID :", bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
-    user_id_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
-    user_id_entry.place(relx=0.5, rely=0.23)
+    tk.Label(top, text="SID: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
+    sid_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    sid_entry.place(relx=0.5, rely=0.23)
 
-    tk.Label(top, text="New Details: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.4)
-    new_details_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
-    new_details_entry.place(relx=0.5, rely=0.43)
+    tk.Label(top, text="Column: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.4)
+    column_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    column_entry.place(relx=0.5, rely=0.43)
+
+    tk.Label(top, text="Value: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.6)
+    value_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    value_entry.place(relx=0.5, rely=0.63)
 
     tk.Button(top, text="Update", font=('Andalus', 15),
               command=update_action, bg=btn_bg_color, width=15,
-              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.8, anchor='center')
+              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.9, anchor='center')
 
 
 def add_book():
@@ -335,27 +431,33 @@ def add_book():
 
 def update_book():
     def update_action():
-        book_id = book_id_entry.get()
-        new_details = new_details_entry.get()
-        # You can add the SQL update command here to update book details
-        log_entries.append("Book details updated")
-        messagebox.showinfo("Info", "Book details updated!")
+        isbn = isbn_entry.get()
+        column = column_entry.get()
+        value = value_entry.get()
+        UpdateBook(isbn, column, value, cnxn)
+        log_entries.append("Book updated")
+        messagebox.showinfo("Info", "Book updated!")
 
     top = tk.Toplevel(root)
     top.title("Update Book")
     top.geometry("400x300")
     top.configure(bg=bg_color)
 
-    tk.Label(top, text="Book ID: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
-    book_id_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
-    book_id_entry.place(relx=0.5, rely=0.23)
-    tk.Label(top, text="New Details: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.4)
-    new_details_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
-    new_details_entry.place(relx=0.5, rely=0.43)
+    tk.Label(top, text="ISBN: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
+    isbn_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    isbn_entry.place(relx=0.5, rely=0.23)
+
+    tk.Label(top, text="Column: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.4)
+    column_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    column_entry.place(relx=0.5, rely=0.43)
+
+    tk.Label(top, text="Value: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.6)
+    value_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    value_entry.place(relx=0.5, rely=0.63)
 
     tk.Button(top, text="Update", font=('Andalus', 15),
               command=update_action, bg=btn_bg_color, width=15,
-              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.8, anchor='center')
+              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.9, anchor='center')
 
 
 def show_books():
@@ -434,7 +536,7 @@ def show_log():
     top.geometry("500x500")
     top.configure(bg=bg_color)
 
-    tk.Label(top, text="Report", bg=bg_color, fg=fg_color, font=('Andalus', 24)).pack(pady=10)
+    tk.Label(top, text="LOG", bg=bg_color, fg=fg_color, font=('Andalus', 24)).pack(pady=10)
 
     log_text = tk.Text(top, font=('Helvetica', 12), bg=btn_bg_color, fg=fg_color, wrap='word', state='normal')
     log_text.pack(expand=True, fill='both')
@@ -538,31 +640,183 @@ def borrowedBooks():
               fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.5, anchor='center')
 
 
+def deleteStud():
+    def delete_action():
+        sid = sid_entry.get()
+        DeleteStud(sid, cnxn)
+        log_entries.append("Student deleted")
+        messagebox.showinfo("Info", "Student deleted!")
+
+    top = tk.Toplevel(root)
+    top.title("Delete Student")
+    top.geometry("400x300")
+    top.configure(bg=bg_color)
+
+    tk.Label(top, text="SID: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
+    sid_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    sid_entry.place(relx=0.5, rely=0.23)
+
+    tk.Button(top, text="Delete", font=('Andalus', 15),
+              command=delete_action, bg=btn_bg_color, width=15,
+              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.8, anchor='center')
+
+
+
+def report():
+    def number_of_books_borrowed_by_all_students():
+
+        data = Number_of_Books_Borrowed_by_AllStuds(cnxn)
+        top = tk.Toplevel(root)
+        top.title("Report")
+        top.geometry("1000x600")
+        top.configure(bg=bg_color)
+
+        tk.Label(top, text="SID", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.1)
+        tk.Label(top, text="Full Name", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.5, rely=0.1)
+        tk.Label(top, text="Number of Borrowed Books", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.8, rely=0.1)
+
+        for i, row in enumerate(data):
+            tk.Label(top, text=row[0], font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2 + i * 0.1)
+            tk.Label(top, text=row[1], font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.5, rely=0.2 + i * 0.1)
+            tk.Label(top, text=row[2], font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.8, rely=0.2 + i * 0.1)
+
+
+
+
+
+
+
+
+
+
+    def number_of_books_written_by_all_authors():
+        data = NumberOfBooksWrittenByAllAuthors(cnxn)
+        top = tk.Toplevel(root)
+        top.title("Report")
+        top.geometry("1500x600")
+        top.configure(bg=bg_color)
+
+        tk.Label(top, text="Author", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.1)
+        tk.Label(top, text="Number of Books", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.8, rely=0.1)
+
+        for i, row in enumerate(data):
+            tk.Label(top, text=row[0], font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2 + i * 0.1)
+            tk.Label(top, text=row[1], font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.8, rely=0.2 + i * 0.1)
+
+
+
+
+
+    top = tk.Toplevel(root)
+    top.title("Report")
+    top.geometry("450x400")
+    top.configure(bg=bg_color)
+    tk.Button(top, text="Number of Books Borrowed by All Students", font=('Andalus', 15),
+                command=number_of_books_borrowed_by_all_students, bg=btn_bg_color, width=35,
+                fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.3, anchor='center')
+
+    tk.Button(top, text="Number of Books Written by All Authors", font=('Andalus', 15),
+                command=number_of_books_written_by_all_authors, bg=btn_bg_color, width=35,
+                fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.5, anchor='center')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def update_admin():
+    def update_action():
+        ssn = ssn_entry.get()
+        column = column_entry.get()
+        value = value_entry.get()
+        UpdateAdmin(ssn, column, value, cnxn)
+        log_entries.append("Admin updated")
+        messagebox.showinfo("Info", "Admin updated!")
+
+    top = tk.Toplevel(root)
+    top.title("Update Admin")
+    top.geometry("400x300")
+    top.configure(bg=bg_color)
+
+    tk.Label(top, text="SSN: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.2)
+    ssn_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    ssn_entry.place(relx=0.5, rely=0.23)
+
+    tk.Label(top, text="Column: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.4)
+    column_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    column_entry.place(relx=0.5, rely=0.43)
+
+    tk.Label(top, text="Value: ", font=("Andalus", 16), bg=bg_color, fg=fg_color).place(relx=0.1, rely=0.6)
+    value_entry = tk.Entry(top, bg=btn_bg_color, font=('Helvetica', 12), fg=fg_color)
+    value_entry.place(relx=0.5, rely=0.63)
+
+    tk.Button(top, text="Update", font=('Andalus', 15),
+              command=update_action, bg=btn_bg_color, width=15,
+              fg=btn_fg_color, activebackground=btn_hover_color).place(relx=0.5, rely=0.9, anchor='center')
+
+
 btn_sign_up = ttk.Button(root, text="Sign Up", command=sign_up)
 btn_update_user = ttk.Button(root, text="Update User", command=update_user)
 btn_add_book = ttk.Button(root, text="Add Book", command=add_book)
 btn_update_book = ttk.Button(root, text="Update Book", command=update_book)
 btn_show_books = ttk.Button(root, text="Show Books", command=show_books)
 btn_browse_books = ttk.Button(root, text="Browse Books", command=browse_books)
-btn_show_log = ttk.Button(root, text="Report", command=show_log)
+btn_show_log = ttk.Button(root, text="Log", command=show_log)
 btn_delete_book = ttk.Button(root, text="Delete Book", command=deleteBook)
 btn_borrow_book = ttk.Button(root, text="Borrow Book", command=borrow)
 btn_borrowed_books = ttk.Button(root, text="Borrowed Books", command=borrowedBooks)
+btn_delete_student = ttk.Button(root, text="Delete Student", command=deleteStud)
+
+btn_report = ttk.Button(root, text="Report", command=report)
+btn_update_admin = ttk.Button(root, text="Update Admin", command=update_admin)
+
 
 button_width = 250
 button_height = 50
+spacing_x = 20
 spacing_y = 40
 
+# First row
 btn_sign_up.place(x=20, y=105, width=button_width, height=button_height)
-btn_update_user.place(x=20, y=105 + button_height + spacing_y, width=button_width, height=button_height)
-btn_add_book.place(x=20, y=105 + 2 * (button_height + spacing_y), width=button_width, height=button_height)
+btn_update_user.place(x=20 + button_width + spacing_x, y=105, width=button_width, height=button_height)
+btn_add_book.place(x=20 + 2 * (button_width + spacing_x), y=105, width=button_width, height=button_height)
 
-btn_update_book.place(x=330, y=105, width=button_width, height=button_height)
-btn_show_books.place(x=330, y=105 + button_height + spacing_y, width=button_width, height=button_height)
-btn_browse_books.place(x=330, y=105 + 2 * (button_height + spacing_y), width=button_width, height=button_height)
-btn_show_log.place(x=20, y=105 + 3 * (button_height + spacing_y), width=button_width, height=button_height)
-btn_delete_book.place(x=330, y=105 + 3 * (button_height + spacing_y), width=button_width, height=button_height)
-btn_borrow_book.place(x=20, y=105 + 4 * (button_height + spacing_y), width=button_width, height=button_height)
-btn_borrowed_books.place(x=330, y=105 + 4 * (button_height + spacing_y), width=button_width, height=button_height)
+# Second row
+btn_update_book.place(x=20, y=105 + button_height + spacing_y, width=button_width, height=button_height)
+btn_show_books.place(x=20 + button_width + spacing_x, y=105 + button_height + spacing_y, width=button_width,
+                     height=button_height)
+btn_browse_books.place(x=20 + 2 * (button_width + spacing_x), y=105 + button_height + spacing_y, width=button_width,
+                       height=button_height)
+
+# Third row
+btn_borrow_book.place(x=20, y=105 + 2 * (button_height + spacing_y), width=button_width, height=button_height)
+btn_borrowed_books.place(x=20 + button_width + spacing_x, y=105 + 2 * (button_height + spacing_y), width=button_width,
+                         height=button_height)
+btn_delete_book.place(x=20 + 2 * (button_width + spacing_x), y=105 + 2 * (button_height + spacing_y),
+                        width=button_width, height=button_height)
+
+
+# Fourth row
+btn_delete_student.place(x=20, y=105 + 3 * (button_height + spacing_y), width=button_width, height=button_height)
+btn_report.place(x=20 + button_width + spacing_x, y=105 + 3 * (button_height + spacing_y), width=button_width,
+                 height=button_height)
+btn_show_log.place(x=20 + 2 * (button_width + spacing_x), y=105 + 3 * (button_height + spacing_y), width=button_width,
+                     height=button_height)
+
+# Fifth row
+btn_update_admin.place(x=20, y=105 + 4 * (button_height + spacing_y), width=button_width, height=button_height)
+
 
 root.mainloop()
